345ae27f3b23d1ad3fa5a62138feff00d28972c7
Lenny Marks <lenny@aps.org>
2020-03-18 11:32:04 -0400

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,25 +1,19 @@
 require_relative 'boot'
 
-require 'rails'
-
-%w(
-  #active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  #active_job/railtie
-  action_cable/engine
-  #rails/test_unit/railtie
-  sprockets/railtie
-).each do |railtie|
- begin
-    require "#{railtie}"
-  rescue LoadError
-  end
-end
-
-# this is here intentionally as part of the exercise. Do not remove or comment out.
-'figure it out'.match?(/\s+/)
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,7 +25,8 @@ module ToyAsis
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
