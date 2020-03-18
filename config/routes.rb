0febf9bf07c907891032922b0b3b5ddbed02eb65
Lenny Marks <lenny@aps.org>
2020-03-18 12:52:52 -0400

    Generate a new rails app over existing
    rails new toy-asis --skip-active-record --skip-active-storage --skip-active-mailbox --skip-bundle --skip-turbolinks --skip-action-cable --skip-action-mailbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  namespace :admin do
+    resources :manuscripts, only: [:index]
+  end
 end
